acf1778b931cb58da2d65d1d2bbc92fdc76a35e2
MightyK <KMightyDev@gmail.com>
2021-03-08 21:07:24 -0500

    Base setup complete

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+    const currentyear = new Date().getFullYear();
+    
+    return <div>
+        <footer>
+            <p>Copyright 2021</p>
+        </footer>
+    </div>;
+};
+
+export default Footer;
