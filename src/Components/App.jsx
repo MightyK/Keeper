eac33af5d1e4d5429273bdebffdb0ea8f699c6ad
MightyK <KMightyDev@gmail.com>
2021-03-09 17:57:48 -0500

    Enabled the use of props

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,12 +2,26 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
+import notes from '../notes';
+
+const createNote = (note) => 
+    (
+        <Note 
+            id={note.key}
+            key={note.key}
+            name={note.title}
+            info={note.content}
+        />
+    );
+
 
 function App() {
     return <div>
         <Header />
+        <div>
+            {notes.map(createNote)}
+        </div>
         <Footer />
-        <Note />
     </div>;
 };
 
