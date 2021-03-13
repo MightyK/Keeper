8c398a9dcca00e7df0279b6598eb192477dc7fd5
MightyK <KMightyDev@gmail.com>
2021-03-12 19:07:19 -0500

    Compontent functionality implemented

--- src/Components/CreateArea.jsx
+++ src/Components/CreateArea.jsx
@@ -2,6 +2,57 @@ import React, {useState} from 'react';
 
 const CreateArea = (props) => {
     
+    const [note, setNote] = useState({
+        title: '',
+        content: ''
+    });
+
+    const {title: noteTitle, content: noteContent} = note;
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+
+        setNote((prevNote) => {
+            return {
+                ...prevNote,
+                [name]: value
+            };
+        });
+    }
+
+    const submitNote = (event) => {
+        // props.onAdd(note)
+        setNote({
+            title: '',
+            content: ''
+        });
+        event.preventDefault();
+    } 
+
+    return (
+        <div>
+            <form>
+                <input 
+                    name='title'
+                    onChange={handleChange}
+                    value={noteTitle}
+                    placeholder='title'    
+                 />
+                <input 
+                    name='content'
+                    onChange={handleChange}
+                    value={noteContent}
+                    placeholder='Write a note...'
+                    rows='3'    
+                 />
+                 <button
+                    onClick={submitNote}    
+                >
+                    Add
+                </button>
+            </form>
+        </div>
+    );
 }
 
 export default CreateArea;
