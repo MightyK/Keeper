12dcc6e4788f4efc2fb77c81983b4df3d5e175c0
MightyK <KMightyDev@gmail.com>
2021-03-12 19:27:04 -0500

    Delete function implemented

--- src/Components/Note.jsx
+++ src/Components/Note.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 
 const Note = props => {
+
+    const handleClick = () => {
+        props.onDelete(props.id);
+    }
+
     return (
     <div className="note">
         <h1>{props.name}</h1>
         <p>{props.info}</p>
-        <button>
+        <button 
+            onClick={handleClick}
+        >
             DELETE
         </button>
     </div>
     );
 };
-
-
 
 export default Note;
