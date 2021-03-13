5f40794bd39cbbff5397d6a05db34443d745c4a9
MightyK <KMightyDev@gmail.com>
2021-03-12 23:14:59 -0500

    Added component to Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
 
 const Header = () => {
     return (
     <div>
         <header>
-            <h1>Keeper</h1>
+            <h1><MenuBookIcon />Keeper</h1>
         </header>
     </div>
     );
