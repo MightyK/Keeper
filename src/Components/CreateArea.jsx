f3452b8939036ce5af318e9cefc10c87171357d6
MightyK <KMightyDev@gmail.com>
2021-03-12 22:15:26 -0500

    Added proper class to element

--- src/Components/CreateArea.jsx
+++ src/Components/CreateArea.jsx
@@ -33,7 +33,7 @@ const CreateArea = (props) => {
 
     return (
         <div>
-            <form>
+            <form className='create-note'>
                 <input 
                     name='title'
                     onChange={handleChange}
